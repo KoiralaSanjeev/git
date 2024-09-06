4fb6f93c7540e32c41398caa0b96791c6fceb349
KoiralaSanjeev <koirala-s@gr.m-fielders.com>
2024-09-06 16:01:50 +0900

    correct code

--- test_1_a_1_12.c
+++ test_1_a_1_12.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void){
+    int i =0;
+    int j = 0;
 
-    int i,j;
-
-    for (i = 1; i < 10; i ++){
+    for(i = 1; i < 10; i++){
         for(j = 1; j < 10; j++){
-            if(i >j){
+            if(i > j){
                 continue;
+                
             }
-            printf("%d * %d = %d\n", i, j, i * j);
+            printf("%d * %d = %d\n", i,j, i * j);
+            
         }
-        
         printf("---------\n");
     }
     return 0;
